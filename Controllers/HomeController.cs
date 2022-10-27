497503a7bcfb8729fccf5a4e592dc04f1a6d55c6
Brandon <brandon.lopes.diaz@gmail.com>
2022-10-27 09:44:58 -0500

    Clase de majo del dia 27/10/2022

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using MCVmma.Models;
+﻿using MCVmma.Context;
+using MCVmma.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,17 +15,25 @@ namespace MCVmma.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var response = await _context.UsuarioDb.ToListAsync();
+            return View(response);
         }
-
         public IActionResult Privacy()
         {
             return View();
